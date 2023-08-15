49026e32505699b0166f3d04590d11d70c99adbf
bartwro <alejary@gmail.com>
2023-08-15 14:08:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    skills.style.display = "block";
+    member.style.display = "none";
+}
